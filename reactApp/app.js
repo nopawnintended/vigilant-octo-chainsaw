f78dcaaf14c4d69f029fb77bd6d14600f2bc0e97
David Huang <davidhuang972@gmail.com>
2017-06-30 20:15:06 -0700

    Add basic todo list

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>Hi!</p>,
-   document.getElementById('root'));
+const dummyData = ['Get to LA', 'Watch TSM Lose', 'Laugh']
+
+class Todo extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <li><button>( Y )</button>{this.props.task}</li>
+    )
+  }
+}
+
+class TodoList extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <ul>
+        {dummyData.map((item) => <Todo task={item}/>)}
+      </ul>
+    )
+  }
+}
+
+class InputLine extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+}
+
+class TodoApp extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+}
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
